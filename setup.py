0f6350bfdaa44092a881d5886f2898feaef1b705
Andrea Zunino <inverseproblem@users.noreply.github.com>
2019-09-04 11:35:58 +0200

    messing around

---
+++ setup.py
@@ -0,0 +1,16 @@
+
+from setuptools import setup
+
+setup(name='kronlininv',
+      version='0.1',
+      python_requires='>=3',
+      description='Kronecker-product based linear inversion',
+      url='http://github.com/inverseproblem/pykronlininv.git',
+      author='Andrea Zunino',
+      author_email='inverseproblem@users.noreply.github.com',
+      license='GPL3',
+      packages=['kronlininv'],
+      install_requires=[
+          'numpy','scipy','numba',
+      ],
+      zip_safe=False)
